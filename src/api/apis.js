53e8ad57ad5aad991463106858ce34273ad5bc9e
Rodger <rodger0531@gmail.com>
2021-05-24 12:37:57 +0800

    add mock data for api

--- src/api/apis.js
+++ src/api/apis.js
@@ -1,4 +1,5 @@
 import api from "./index";
+import mockData from "../dev/Day_Confirmation_Age_County_Gender_19CoV.json";
 
 const useProxy = (url) => {
   return `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`;
@@ -6,15 +7,15 @@ const useProxy = (url) => {
 
 const apis = {
   stats: {
-    getConfirmedCases: () =>
-      api({
-        method: "GET",
-        // Use proxy to get around CORS
-        url: (() =>
-          useProxy(
-            "https://od.cdc.gov.tw/eic/Day_Confirmation_Age_County_Gender_19CoV.json"
-          ))(),
-      }).then(({ data }) => JSON.parse(data.contents)),
+    getConfirmedCases: () => mockData,
+    // api({
+    //   method: "GET",
+    //   // Use proxy to get around CORS
+    //   url: (() =>
+    //     useProxy(
+    //       "https://od.cdc.gov.tw/eic/Day_Confirmation_Age_County_Gender_19CoV.json"
+    //     ))(),
+    // }).then(({ data }) => JSON.parse(data.contents)),
   },
 };
 

39e661da90efe2a460882dbe9d1182a6d8d23166
Rodger <rodger0531@gmail.com>
2021-05-24 12:38:34 +0800

    add charts for data

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,10 +3,8 @@ import ConfirmedCases from "./confirmedCases";
 
 function App(props) {
   return (
-    <div className="App">
-      <header className="text-white bg-gray-800">
-        <ConfirmedCases />
-      </header>
+    <div className="App p-10">
+      <ConfirmedCases />
     </div>
   );
 }
--- src/containers/confirmedCases.js
+++ src/containers/confirmedCases.js
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getConfirmedCasesListState } from "../redux/selectors";
 import { requestConfirmedCasesList } from "../redux/actions";
+import LineChart from "../components/lineChart";
+import BarChart from "../components/barChart";
 
 const ConfirmedCases = (props) => {
   const dispatch = useDispatch();
@@ -17,26 +19,39 @@ const ConfirmedCases = (props) => {
       (sum += value)
   )(0);
 
+  const cumulateData =
+    confirmedCases &&
+    Object.entries(
+      confirmedCases?.reduce((t, x) => {
+        t[x.個案研判日] = {
+          count: (t[x.個案研判日]?.count || 0) + +x.確定病例數,
+          cumSum: cumulativeSum(+x.確定病例數),
+        };
+        return t;
+      }, {})
+    );
+
   return (
-    <div className="text-sm">
+    <div className="text-sm flex flex-col items-center">
       {isLoading && <p>Loading</p>}
       {confirmedCases && (
+        <div className="w-3/5 flex flex-col">
+          <LineChart
+            data={cumulateData}
+            title="Covid-19 確診數"
+            className="mb-4"
+          />
+          <hr className="my-8" />
+          <BarChart data={cumulateData} title="Covid-19 確診數" className="" />
+        </div>
+      )}
+      {confirmedCases && (
         <>
-          {Object.entries(
-            confirmedCases.reduce((t, x) => {
-              t[x.個案研判日] = {
-                count: (t[x.個案研判日]?.count || 0) + +x.確定病例數,
-                cumSum: cumulativeSum(+x.確定病例數),
-              };
-              return t;
-            }, {})
-          )
-            .reverse()
-            .map(([key, { count, cumSum }]) => (
-              <p key={key}>
-                {key}: {count}, sum: {cumSum}
-              </p>
-            ))}
+          {cumulateData.reverse().map(([key, { count, cumSum }]) => (
+            <p key={key}>
+              {key}: {count}, sum: {cumSum}
+            </p>
+          ))}
         </>
       )}
     </div>

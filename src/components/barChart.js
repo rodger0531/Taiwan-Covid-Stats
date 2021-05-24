8f484f6755422201d4ab10cdba12cd320be58ba7
Rodger <rodger0531@gmail.com>
2021-05-24 12:38:24 +0800

    add bar and line chart component

---
+++ src/components/barChart.js
@@ -0,0 +1,58 @@
+import React from "react";
+import blue from "@material-ui/core/colors/indigo";
+import CanvasJSReact from "../assets/canvasjs.react";
+
+var CanvasJS = CanvasJSReact.CanvasJS;
+CanvasJS.addColorSet("materialUI", [
+  blue[900],
+  blue[800],
+  blue[600],
+  blue[500],
+  blue[300],
+  blue[200],
+  blue[100],
+]);
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+const BarChart = ({ data, title, className }) => {
+  data = data.slice(0, 7);
+
+  const options = {
+    animationEnabled: true,
+    title: {
+      text: title,
+    },
+    axisY: {
+      includeZero: true,
+    },
+    axisX: {
+      valueFormatString: "MM/DD",
+    },
+    colorSet: "materialUI",
+    toolTip: {
+      contentFormatter: function (e) {
+        return `確診數: ${e.entries[0].dataPoint.y}`;
+      },
+    },
+    culture: "zh",
+    data: [
+      {
+        type: "column", //change type to bar, line, area, pie, etc
+        //indexLabel: "{y}", //Shows y value on all Data Points
+        indexLabelFontColor: "#5A5757",
+        indexLabelPlacement: "outside",
+        dataPoints: data.map(([key, value]) => ({
+          x: new Date(key),
+          y: value.count,
+        })),
+      },
+    ],
+  };
+  return (
+    <div className={className}>
+      <CanvasJSChart options={options} />
+    </div>
+  );
+};
+
+export default BarChart;
---
+++ src/components/lineChart.js
@@ -0,0 +1,56 @@
+import React from "react";
+import CanvasJSReact from "../assets/canvasjs.react";
+
+// var CanvasJS = CanvasJSReact.CanvasJS;
+// CanvasJS.addCultureInfo("zh", {
+//   shortMonths: [
+//     "一月",
+//     "二月",
+//     "三月",
+//     "四月",
+//     "五月",
+//     "六月",
+//     "七月",
+//     "八月",
+//     "九月",
+//     "十月",
+//     "十一月",
+//     "十二月",
+//   ],
+// });
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+const LineChart = ({ data, title, className = "" }) => {
+  const options = {
+    animationEnabled: true,
+    title: {
+      text: title,
+    },
+    axisY: {
+      includeZero: true,
+    },
+    // culture: "zh",
+    data: [
+      {
+        type: "line", //change type to bar, line, area, pie, etc
+        //indexLabel: "{y}", //Shows y value on all Data Points
+        indexLabelFontColor: "#5A5757",
+        indexLabelPlacement: "outside",
+        dataPoints: data.map(([key, value]) => ({
+          x: new Date(key),
+          y: value.cumSum,
+        })),
+      },
+    ],
+  };
+  return (
+    <div className={className}>
+      <CanvasJSChart
+        options={options}
+        /* onRef={ref => this.chart = ref} */
+      />
+    </div>
+  );
+};
+
+export default LineChart;

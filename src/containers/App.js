b4b8223ed6137cd4ab1bac0f951bc01fcd5bb3fc
Rodger <rodger0531@gmail.com>
2021-05-25 01:14:18 +0800

    add sample tab menu for charts

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,9 @@
 // import "./App.css";
 import ConfirmedCases from "./confirmedCases";
-// import { Container } from "@material-ui/core";
+import { Container, Typography, Link } from "@material-ui/core";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import GeneralStats from "./generalStats";
+import ChartTabs from "./chartTabs";
 
 const theme = createMuiTheme({
   palette: {
@@ -14,8 +15,8 @@ const theme = createMuiTheme({
     },
     secondary: {
       light: "#97e6ff",
-      main: "#60b4f4",
-      dark: "#1985c1",
+      main: "#1976d2",
+      dark: "#0d47a1",
       contrastText: "#000000",
     },
   },
@@ -25,6 +26,22 @@ function App(props) {
   return (
     <ThemeProvider theme={theme}>
       <div className="p-10 bg-gray-100">
+        <Container>
+          <Typography align="center" variant="h4" component="h2" gutterBottom>
+            台灣疫情統計
+          </Typography>
+          <Typography
+            align="center"
+            variant="h6"
+            color="textSecondary"
+            gutterBottom
+          >
+            <Link href="https://www.cdc.gov.tw" color="secondary">
+              {"{"} 數據來源: 衛福部疾管署 {"}"}
+            </Link>
+          </Typography>
+        </Container>
+        <ChartTabs />
         <GeneralStats />
         <ConfirmedCases />
       </div>
---
+++ src/containers/chartTabs.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { Paper, Tab, Tabs, Typography, Box, AppBar } from "@material-ui/core";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-auto-tabpanel-${index}`}
+      aria-labelledby={`scrollable-auto-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+const ChartTabs = () => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <Paper elevation={0}>
+      <AppBar position="static" color="secondary">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          centered
+        >
+          <Tab label="Item One" />
+          <Tab label="Item Two" />
+          <Tab label="Item Three" />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+    </Paper>
+  );
+};
+
+export default ChartTabs;
